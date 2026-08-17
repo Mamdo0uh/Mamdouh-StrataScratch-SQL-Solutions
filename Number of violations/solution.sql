f8bfdffd31b7d3667c0ce1fb8e36e7a6f2ebc975
Mohammed Mamdouh <mamdouhd845@gmail.com>
2026-08-17 23:31:45 +0300

    add solution to Number of violations

---
+++ Number of violations/solution.sql
@@ -0,0 +1,4 @@
+select year(inspection_date) as inspection_year ,count(violation_id) as n_violations
+from sf_restaurant_health_violations
+where business_name ='Roxanne Cafe'
+group by year(inspection_date)
